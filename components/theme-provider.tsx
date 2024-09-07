349aed85de79ee57d6b927a49bbbe5c8c5056d74
tobiolatiloGit24 <olatilotobi@gmail.com>
2024-09-07 19:59:36 +0100

    add shadcn ui and theme

---
+++ components/theme-provider.tsx
@@ -0,0 +1,12 @@
+"use client";
+/**
+ * We create the Provider theme...
+ * Then we wrap our application with the provider theme component.
+ */
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
