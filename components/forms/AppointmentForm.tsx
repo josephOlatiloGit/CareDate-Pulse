0a08065c6a138eb37f7c7a47fcf2038657f8eac0
tobiolatiloGit24 <olatilotobi@gmail.com>
2024-11-09 14:56:33 +0100

    update appointment  form validation

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -57,9 +57,11 @@ export default function AppointmentForm({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
       primaryPhysician: appointment ? appointment.primaryPhysician : "",
-      schedule: appointment ? new Date(appointment.schedule) : new Date(),
+      schedule: appointment
+        ? new Date(appointment.schedule)
+        : new Date(Date.now()),
       reason: appointment ? appointment.reason : "",
-      note: appointment ? appointment.note : "",
+      note: appointment?.note || "",
       cancellationReason: appointment?.cancellationReason || "",
     },
   });
